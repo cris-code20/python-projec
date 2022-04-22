0c14938378f9976b0c4734c2d405c8e9dcbf9ff2
bubu <cris.cadosh@gmail.com>
2022-04-22 11:03:06 -0400

    gregamos el admin

--- libreria/app/models.py
+++ libreria/app/models.py
@@ -1,3 +1,22 @@
+from distutils.command.upload import upload
+from email.mime import image
+from tokenize import blank_re
 from django.db import models
 
 # Create your models here.
+
+class Libro(models.Model):
+    id     = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=100)
+    image  = models.ImageField(upload_to='imagen/', verbose_name="imagen", null=True)
+    descricion = models.TextField(verbose_name="descricion" ,null=True)
+    # autor  = models.CharField(max_length=100)
+    # genero = models.CharField(max_length=100)
+    # anio   = models.IntegerField()
+    # editor = models.CharField(max_length=100)
+    # isbn   = models.CharField(max_length=100)
+    # prezzo = models.FloatField()
+    # disponibilidad = models.IntegerField()
+
+    # def __str__(self):
+    #     return self.titulo
